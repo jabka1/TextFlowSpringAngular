a0e178620e7117e7d0c0a03cfe863208d2927cd5
Artem Romaniuk <artemromaniuk4@gmail.com>
2025-03-27 16:47:53 +0200

    Init project

---
+++ src/main/java/team/textflowspringangular/TextFlowSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package team.textflowspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextFlowSpringAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextFlowSpringAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/textflowspringangular/TextFlowSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package team.textflowspringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextFlowSpringAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
